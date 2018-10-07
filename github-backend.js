4433d9e1dd99f923da0df9cdd7a9c6493338cf05
Hourhawk <ryanhuellen@icloud.com>
2018-10-07 15:23:58 -0500

    Case 9 (Word Communication)

--- github-backend.js
+++ github-backend.js
@@ -1,1 +1,7 @@
 var socket = io('https://github-backend.tk');
+socket.on('update-word', function(word) {
+	$('h2').html('The current word is: ' + word);
+});
+$('button').click(function() {
+	socket.emit('update-word', $('input').val());
+});
